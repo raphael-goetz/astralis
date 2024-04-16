5bb39c19c493c7269b7cf5de4f95af095dae8025
Raphael <raphael.goetz.7@gmail.com>
2024-04-16 20:47:45 +0200

    initial commit

---
+++ src/main/kotlin/de/raphaelgoetz/astralis/Astralis.kt
@@ -0,0 +1,13 @@
+package de.raphaelgoetz.astralis
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Astralis : JavaPlugin() {
+
+    override fun onLoad() {}
+
+    override fun onEnable() {}
+
+    override fun onDisable() {}
+
+}
