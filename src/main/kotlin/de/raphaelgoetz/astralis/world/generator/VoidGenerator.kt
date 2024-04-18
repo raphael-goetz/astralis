b3a549091e0e586af37d4cbb44e9b6ba337fa089
Raphael <raphael.goetz.7@gmail.com>
2024-04-18 22:07:35 +0200

    feat: simple void generation

---
+++ src/main/kotlin/de/raphaelgoetz/astralis/world/generator/VoidGenerator.kt
@@ -0,0 +1,25 @@
+package de.raphaelgoetz.astralis.world.generator
+
+import org.bukkit.Material
+import org.bukkit.generator.ChunkGenerator
+import org.bukkit.generator.WorldInfo
+import java.util.Random
+
+/**
+ * A simple void-generator that will create a bedrock block at x=0, y=64, z=0
+ */
+class VoidGenerator : ChunkGenerator() {
+
+    override fun generateBedrock(worldInfo: WorldInfo, random: Random, chunkX: Int, chunkZ: Int, chunkData: ChunkData) {
+
+        val x = 0;
+        val y = 64;
+        val z = 0
+
+        if ((x >= chunkX * 16) && (x < (chunkX + 1) * 16)) {
+            if ((z >= chunkZ * 16) && (z < (chunkZ + 1) * 16)) {
+                chunkData.setBlock(x, y, z, Material.BEDROCK);
+            }
+        }
+    }
+}
