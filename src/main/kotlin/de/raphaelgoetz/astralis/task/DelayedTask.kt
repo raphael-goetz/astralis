3979bc897c7ff41568c37e7eece8b3fbed1d6531
Raphael <raphael.goetz.7@gmail.com>
2024-04-19 21:11:51 +0200

    fix: function now will be called after the given delay

--- src/main/kotlin/de/raphaelgoetz/astralis/task/DelayedTask.kt
+++ src/main/kotlin/de/raphaelgoetz/astralis/task/DelayedTask.kt
@@ -1,10 +1,10 @@
 package de.raphaelgoetz.astralis.task
 
-import de.raphaelgoetz.astralis.annotations.InternalUse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.*
+import kotlin.concurrent.schedule
 
 /**
  * @param delay is the time after which the given function gets executed
@@ -13,8 +13,10 @@ import java.util.*
  */
 inline fun doLater(
     delay: Long,
-    function: () -> Unit = {}
-) = DelayedTaskBuilder(false, delay).apply { function() }.execute()
+    crossinline function: () -> Unit = {}
+) = Timer(false).schedule(delay, ) {
+    function.invoke()
+}
 
 /**
  * @param delay is the time after which the given function gets executed
@@ -23,37 +25,7 @@ inline fun doLater(
  */
 inline fun doLaterAsync(
     delay: Long,
-    function: () -> Unit = {}
-) = DelayedTaskBuilder(true, delay).apply { function() }.execute()
-
-@InternalUse
-data class DelayedTaskBuilder(
-    private val async: Boolean,
-    private val delay: Long,
-    private val function: () -> Unit = {}
-) {
-
-    private val timer = Timer()
-
-    fun execute() = timer.schedule(createTask(async, function), delay)
-
-}
-
-@InternalUse
-private fun createTask(
-    async: Boolean,
-    function: () -> Unit
-): TimerTask {
-
-    return object : TimerTask() {
-
-        override fun run() {
-
-            if (async) {
-                CoroutineScope(Dispatchers.Default).launch { function() }
-            } else {
-                function()
-            }
-        }
-    }
+    crossinline function: () -> Unit = {}
+) = Timer().schedule(delay) {
+    CoroutineScope(Dispatchers.Default).launch { function.invoke() }
 }
