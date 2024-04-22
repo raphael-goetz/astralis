33e5462bec24e6dd3f5232362b7df94b07708043
Raphael <raphael.goetz.7@gmail.com>
2024-04-22 21:24:05 +0200

    feat: added item only click/interact listener

--- src/main/kotlin/de/raphaelgoetz/astralis/event/Event.kt
+++ src/main/kotlin/de/raphaelgoetz/astralis/event/Event.kt
@@ -2,7 +2,10 @@ package de.raphaelgoetz.astralis.event
 
 import org.bukkit.entity.Player
 import org.bukkit.event.*
+import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 fun Listener.unregister() = HandlerList.unregisterAll(this)
@@ -35,8 +38,23 @@ inline fun <reified T : PlayerEvent> Player.listen(
     return wrapper
 }
 
-inline fun <reified T : Event> listenCancelled(plugin: JavaPlugin) : Listener =
+inline fun <reified T : Event> listenCancelled(plugin: JavaPlugin): Listener =
     listen<T>(plugin, EventPriority.LOWEST) { event ->
         if (event is Cancellable) event.isCancelled = true
     }
 
+inline fun ItemStack.listenClick(
+    plugin: JavaPlugin,
+    priority: EventPriority = EventPriority.NORMAL,
+    crossinline onEvent: (event: InventoryClickEvent) -> Unit,
+) = listen<InventoryClickEvent>(plugin, priority) { event ->
+    if (event.clickedInventory == this) onEvent.invoke(event)
+}
+
+inline fun ItemStack.listenInteract(
+    plugin: JavaPlugin,
+    priority: EventPriority = EventPriority.NORMAL,
+    crossinline onEvent: (event: PlayerInteractEvent) -> Unit
+) = listen<PlayerInteractEvent>(plugin, priority) { event ->
+    if (event.item == this) onEvent.invoke(event)
+}
