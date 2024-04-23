d6a0897287a94310b31436750dc9b8267ef89adc
Raphael <raphael.goetz.7@gmail.com>
2024-04-23 20:24:11 +0200

    feat: added placeholder enum for inventory-size

---
+++ src/main/kotlin/de/raphaelgoetz/astralis/ui/data/InventoryRows.kt
@@ -0,0 +1,14 @@
+package de.raphaelgoetz.astralis.ui.data
+
+/**
+ *  Placeholder for default inventory-rows
+ *  @param value is the corresponding inventory size
+ */
+enum class InventoryRows(val value: Int) {
+    ROW1(9),
+    ROW2(18),
+    ROW3(27),
+    ROW4(36),
+    ROW5(45),
+    ROW6(54),
+}
