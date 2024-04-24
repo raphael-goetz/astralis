2efb29d65c307c8b7aebdb3fd434fdb092c42fe3
Raphael <raphael.goetz.7@gmail.com>
2024-04-24 22:16:27 +0200

    feat: implemented functions for adding/removing items to InventoryBuilder

--- src/main/kotlin/de/raphaelgoetz/astralis/ui/builder/InventoryBuilder.kt
+++ src/main/kotlin/de/raphaelgoetz/astralis/ui/builder/InventoryBuilder.kt
@@ -1,26 +1,73 @@
 package de.raphaelgoetz.astralis.ui.builder
 
 import de.raphaelgoetz.astralis.event.listen
+import de.raphaelgoetz.astralis.items.builder.SmartItem
 import de.raphaelgoetz.astralis.ui.data.InventoryRows
+import de.raphaelgoetz.astralis.ui.data.InventorySlots
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
-import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.Inventory
 import org.bukkit.plugin.java.JavaPlugin
+
 import java.util.function.Consumer
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class InventoryBuilder(
     javaPlugin: JavaPlugin,
     title: Component,
-    holder: Player,
+    private val holder: Player,
     rows: InventoryRows = InventoryRows.ROW6,
 ) {
 
-    private val clickEvents: HashMap<Int, Consumer<InventoryClickEvent>> = HashMap()
+    private val clickEvents: HashMap<Int, SmartClick> = HashMap()
     private val closeEvents: ArrayList<Runnable> = arrayListOf()
     private val inventory: Inventory = Bukkit.createInventory(holder, rows.value, title)
+
+    fun clear() {
+        clickEvents.clear()
+        inventory.clear()
+    }
+
+    fun open() = holder.openInventory(inventory)
+
+    fun setSlot(slot: InventorySlots, item: SmartItem, action: Consumer<InventoryClickEvent>?) {
+        if (action != null) this.clickEvents[slot.value] = SmartClick(item, action)
+        inventory.setItem(slot.value, item.itemStack)
+    }
+
+    fun setSlot(slot: InventorySlots, item: SmartItem) = setSlot(slot, item, null)
+
+    fun setBlockedSlot(slot: InventorySlots, item: SmartItem, action: Consumer<InventoryClickEvent>?) {
+        setSlot(slot, item) { event ->
+            event.isCancelled = true
+            action?.accept(event)
+        }
+    }
+
+    fun setBlockedSlot(slot: InventorySlots, item: SmartItem) = setBlockedSlot(slot, item, null)
+
+    fun addSlot(item: SmartItem, action: Consumer<InventoryClickEvent>?) {
+        inventory.addItem(item.itemStack)
+        val slot = inventory.first(item.itemStack)
+        if (action != null) this.clickEvents[slot] = SmartClick(item, action)
+    }
+
+    fun addSlot(item: SmartItem) = addSlot(item, null)
+
+    fun addBlockedSlot(item: SmartItem, action: Consumer<InventoryClickEvent>?) {
+        addSlot(item) { event ->
+            event.isCancelled = true
+            action?.accept(event)
+        }
+    }
+
+    fun removeSlot(slot: InventorySlots) {
+        inventory.clear(slot.value)
+        clickEvents.remove(slot.value)
+    }
 
     init {
         listen<InventoryClickEvent>(javaPlugin) { inventoryClickEvent ->
@@ -31,8 +78,9 @@ class InventoryBuilder(
             val slot: Int = inventoryClickEvent.slot
             if (inventoryClickEvent.clickedInventory!! != inventory) return@listen
 
-            clickEvents[slot]?.accept(inventoryClickEvent)
-            holder.playSound(holder, Sound.UI_BUTTON_CLICK, 1f, 1f)
+            val smartClick = clickEvents[slot] ?: return@listen
+            smartClick.action.accept(inventoryClickEvent)
+            holder.playSound(holder, smartClick.item.interactionType.sound, 1f, 1f)
         }
     }
 }
