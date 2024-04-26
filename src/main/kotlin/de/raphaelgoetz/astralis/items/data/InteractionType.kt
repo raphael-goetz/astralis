c8f2b70a75f4a73815b5e867f93b67386322bc9e
Raphael <raphael.goetz.7@gmail.com>
2024-04-26 23:01:41 +0200

    feat: added PAGE_TURN to InteractionType

--- src/main/kotlin/de/raphaelgoetz/astralis/items/data/InteractionType.kt
+++ src/main/kotlin/de/raphaelgoetz/astralis/items/data/InteractionType.kt
@@ -13,5 +13,6 @@ enum class InteractionType(val color: Colorization, val sound: Sound) {
     ERROR(Colorization.RED, Sound.BLOCK_ANVIL_BREAK),
     ENABLED(Colorization.LIME, Sound.ENTITY_VILLAGER_YES),
     DISABLED(Colorization.RED, Sound.ENTITY_VILLAGER_NO),
-    CLICK(Colorization.GRAY, Sound.UI_LOOM_TAKE_RESULT)
+    CLICK(Colorization.GRAY, Sound.UI_LOOM_SELECT_PATTERN),
+    PAGE_TURN(Colorization.GRAY, Sound.UI_LOOM_TAKE_RESULT)
 }
