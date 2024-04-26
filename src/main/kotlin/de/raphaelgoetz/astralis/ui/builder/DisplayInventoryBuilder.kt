9589b5428df5e39ffd810a3b9cd0d70fd6be88e5
Raphael <raphael.goetz.7@gmail.com>
2024-04-26 23:05:26 +0200

    fix: fixed logic for page turning

--- src/main/kotlin/de/raphaelgoetz/astralis/ui/builder/DisplayInventoryBuilder.kt
+++ src/main/kotlin/de/raphaelgoetz/astralis/ui/builder/DisplayInventoryBuilder.kt
@@ -2,10 +2,12 @@ package de.raphaelgoetz.astralis.ui.builder
 
 import com.google.common.collect.Lists
 import de.raphaelgoetz.astralis.items.builder.SmartItem
+import de.raphaelgoetz.astralis.items.data.InteractionType
 import de.raphaelgoetz.astralis.ui.data.InventoryRows
 import de.raphaelgoetz.astralis.ui.data.InventorySlots
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 class DisplayInventoryBuilder(
@@ -18,7 +20,8 @@ class DisplayInventoryBuilder(
     private val to: InventorySlots
 ) : InventoryBuilder(javaPlugin, title, holder, rows) {
 
-    private val maxItems = from.value - to.value
+    //to should be inclusive. That's why to + 1
+    private val maxItems: Int = (to.value + 1) - from.value
     private val pages = Lists.partition(list, maxItems)
     private val maxPage = pages.count()
 
@@ -31,27 +34,36 @@ class DisplayInventoryBuilder(
     private fun applyPage() {
 
         val currentPage = pages[currentPageIndex]
-        var currentItemIndex = 0
 
-        for (index in from.value until to.value) {
-            val currentItem = currentPage[currentPageIndex]
+        for (index in from.value until (to.value + 1)) {
+            if (index >= currentPage.size) break
+            val currentItem = currentPage[index]
             setSlot(index, currentItem.item, currentItem.action)
-            currentItemIndex++
         }
     }
 
-    fun pageLeft(slot: InventorySlots, smartItem: SmartItem) {
-        this.setBlockedSlot(slot, smartItem) {
-            if (currentPageIndex - 1 <= 0) return@setBlockedSlot
+    fun pageLeft(slot: InventorySlots, display: ItemStack) {
+        this.setBlockedSlot(slot, SmartItem(display, InteractionType.PAGE_TURN)) {
+            if (currentPageIndex - 1 < 0) {
+                currentPageIndex = (maxPage - 1)
+                applyPage()
+                return@setBlockedSlot
+            }
             currentPageIndex--
             applyPage()
         }
     }
 
-    fun pageRight(slot: InventorySlots, smartItem: SmartItem) {
-        this.setBlockedSlot(slot, smartItem) {
-            if (currentPageIndex + 1 >= maxPage) return@setBlockedSlot
+    fun pageRight(slot: InventorySlots, display: ItemStack) {
+        this.setBlockedSlot(slot, SmartItem(display, InteractionType.PAGE_TURN)) {
+            if (currentPageIndex + 1 >= maxPage) {
+                currentPageIndex = 0
+                println(currentPageIndex)
+                applyPage()
+                return@setBlockedSlot
+            }
             currentPageIndex++
+            println(currentPageIndex)
             applyPage()
         }
     }
