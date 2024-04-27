6405830215c49347614c7c737997251373a8a8f7
Raphael <raphael.goetz.7@gmail.com>
2024-04-27 15:11:10 +0200

    ref: moved Communicator
    Signed-off-by: raph <raohael.goetz.7@gmail.com>

--- src/main/kotlin/de/raphaelgoetz/astralis/text/Communicator.kt
+++ src/main/kotlin/de/raphaelgoetz/astralis/text/Communicator.kt
@@ -1,18 +1,22 @@
-package de.raphaelgoetz.astralis.text.communication
+package de.raphaelgoetz.astralis.text
 
+import de.raphaelgoetz.astralis.schedule.doAgain
+import de.raphaelgoetz.astralis.schedule.doAgainAsync
 import de.raphaelgoetz.astralis.text.components.AdventureMessage
 import de.raphaelgoetz.astralis.text.components.ComponentBuilder
 import de.raphaelgoetz.astralis.text.components.adventureMessage
+import de.raphaelgoetz.astralis.text.components.adventureText
 import de.raphaelgoetz.astralis.text.title.AdventureTitle
 import de.raphaelgoetz.astralis.text.title.TitleBuilder
 import de.raphaelgoetz.astralis.text.title.adventureTitle
+import net.kyori.adventure.text.Component
 
 import org.bukkit.entity.Player
 
 /**
- * Sends the player a component & plays a sound at the same time
- * @param text is the text that will be converted to a component that the player will receive
- * @param builder is the ComponentBuilder which contains the sound and the styling
+ * Sends the player a component & plays a sound at the same time.
+ * @param text is the text that will be converted to a component that the player will receive.
+ * @param builder is the ComponentBuilder which contains the sound and the styling.
  */
 fun Player.sendText(
     text: String = "",
@@ -20,8 +24,8 @@ fun Player.sendText(
 ) = this.sendText(adventureMessage(text, builder))
 
 /**
- * Sends the player a component & plays a sound at the same time
- * @param adventureMessage is the component wrapper that contains the styled component and the sound
+ * Sends the player a component & plays a sound at the same time.
+ * @param adventureMessage is the component wrapper that contains the styled component and the sound.
  */
 fun Player.sendText(adventureMessage: AdventureMessage) {
     this.sendMessage(adventureMessage.component)
@@ -29,8 +33,8 @@ fun Player.sendText(adventureMessage: AdventureMessage) {
 }
 
 /**
- * Sends the player a title & plays a sound at the same time
- * @param adventureTitle is the title wrapper that contains the styled title and the sound
+ * Sends the player a title & plays a sound at the same time.
+ * @param adventureTitle is the title wrapper that contains the styled title and the sound.
  */
 fun Player.sendTitle(adventureTitle: AdventureTitle) {
     this.showTitle(adventureTitle.title)
@@ -38,10 +42,10 @@ fun Player.sendTitle(adventureTitle: AdventureTitle) {
 }
 
 /**
- * Sends the player a title & plays a sound at the same time
- * @param title is the content of the upper title
- * @param subTitle is the content of the lower title
- * @param builder is the TitleBuilder which contains the sound and the styling
+ * Sends the player a title & plays a sound at the same time.
+ * @param title is the content of the upper title.
+ * @param subTitle is the content of the lower title.
+ * @param builder is the TitleBuilder which contains the sound and the styling.
  */
 fun Player.sendTitle(
     title: String,
