f47b12e87434714fe1503be0112bb397fde0d560
Raphael <raphael.goetz.7@gmail.com>
2024-04-28 14:14:02 +0200

    docs: added for annotation
    Signed-off-by: Raphael <raphael.goetz.7@gmail.com>

--- src/main/kotlin/de/raphaelgoetz/astralis/annotations/InternalUse.kt
+++ src/main/kotlin/de/raphaelgoetz/astralis/annotations/InternalUse.kt
@@ -1,3 +1,6 @@
 package de.raphaelgoetz.astralis.annotations
 
+/**
+ * When a function/class is marked with this annotation. It shouldn't be used by the plugin that is implementing it.
+ */
 annotation class InternalUse
