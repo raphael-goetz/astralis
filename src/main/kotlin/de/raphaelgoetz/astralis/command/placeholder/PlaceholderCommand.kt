e7325500daa87a0d3aacce12c0aaead2c40c8d93
Raphael <raphael.goetz.7@gmail.com>
2024-05-02 21:28:49 +0200

    feat: added placeholder command to trick paper

---
+++ src/main/kotlin/de/raphaelgoetz/astralis/command/placeholder/PlaceholderCommand.kt
@@ -0,0 +1,12 @@
+package de.raphaelgoetz.astralis.command.placeholder
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+
+class PlaceholderCommand(label: String) : Command(label) {
+
+    override fun execute(p0: CommandSender, p1: String, p2: Array<out String>?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+}
