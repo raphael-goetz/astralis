15a81d0d5514b8ef7c2096e31fbcf1ebb8921ded
Raphael <raphael.goetz.7@gmail.com>
2024-05-03 23:04:21 +0200

    feat: Location table

--- src/main/kotlin/de/raphaelgoetz/astralis/database/TablePresets.kt
+++ src/main/kotlin/de/raphaelgoetz/astralis/database/TablePresets.kt
@@ -4,16 +4,39 @@ import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.jodatime.CurrentDateTime
 import org.jetbrains.exposed.sql.jodatime.datetime
 
-class PlayerTable : Table() {
+open class PlayerTable : Table() {
     val uuid = uuid("uuid")
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
 
     override val primaryKey = PrimaryKey(uuid)
 }
 
-class DataTable : Table() {
+open class DataTable : Table() {
     val id = integer("id").autoIncrement()
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
 
     override val primaryKey = PrimaryKey(id)
+}
+
+open class PlayerLocationTable : LocationTable() {
+    val uuid = uuid("uuid")
+    val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
+
+    override val primaryKey = PrimaryKey(uuid)
+}
+
+open class DataLocationTable : LocationTable() {
+    val id = integer("id").autoIncrement()
+    val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
+
+    override val primaryKey = PrimaryKey(id)
+}
+
+open class LocationTable : Table() {
+    val world = short("world")
+    val x = double("x")
+    val y = double("y")
+    val z = double("z")
+    val pitch = float("pitch")
+    val yaw = float("yaw")
 }
